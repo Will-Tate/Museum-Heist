86e072c08eae6f546f9c6a9e0e7325abc8eb4cdc
Will-Tate <103454866+Will-Tate@users.noreply.github.com>
2024-04-18 18:36:57 -0400

    set up animations, glitchy hiding

--- scenes/mainCharacter.gd
+++ scenes/mainCharacter.gd
@@ -6,8 +6,10 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var canHide = false;
+var canHide = true;
 var canSee = true;
+var canMove = true;
+@onready var sprite_2d = $Sprite2D
 
 
 func _physics_process(delta):
@@ -16,27 +18,35 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 		
 	if Input.is_action_just_pressed("hide_behind") and is_on_floor() and canHide:
 		hideBehind();
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if canMove:
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
+		move_and_slide()
+		if velocity.x !=0 and sprite_2d.animation !="walk":
+			sprite_2d.animation ="walk"
+		if velocity.x ==0 and canSee == true:
+			sprite_2d.animation = "default"
+		var isLeft = velocity.x <0
+		sprite_2d.flip_h = isLeft
 	
 func hideBehind():
 	#swap sprites to hiding
 	velocity.x = 0;
 	velocity.y = 0;
-	while Input.is_action_pressed("hide_behind"):
+	sprite_2d.animation = "Hide"
+	canMove = false
+	while !Input.is_action_just_released("hide_behind"):
 		canSee = false;
 	canSee = true;
+	canMove = true
+	sprite_2d.animation = "default"
 	#when released swap back sprite, reenable movement and detection 
