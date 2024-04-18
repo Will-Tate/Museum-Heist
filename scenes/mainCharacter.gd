80524b0e131ea9bca8476cf06e2642bc787eb008
Will-Tate <103454866+Will-Tate@users.noreply.github.com>
2024-04-18 00:53:03 -0400

    Worked on hiding and set up controls
    Just needs to add in swapping sprites to the hideBehind method

--- scenes/mainCharacter.gd
+++ scenes/mainCharacter.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var canHide = false;
+var canSee = true;
 
 
 func _physics_process(delta):
@@ -16,7 +18,9 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_just_pressed("hide_behind") and is_on_floor() and canHide:
+		hideBehind();
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -26,3 +30,13 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
+func hideBehind():
+	#swap sprites to hiding
+	velocity.x = 0;
+	velocity.y = 0;
+	while Input.is_action_pressed("hide_behind"):
+		canSee = false;
+	canSee = true;
+	#when released swap back sprite, reenable movement and detection 
