dfacfbdc9cd8b463892437600c1329f479af4587
Will-Tate <103454866+Will-Tate@users.noreply.github.com>
2024-04-21 01:08:16 -0400

    Can hide behind every object

---
+++ scenes/museum_item_5.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+var player
+var player_in_area = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("getCanSee"):
+		#go to victory scene
+		player_in_area= true
+		player = body
+		player.setCanHide(true)
+		
+
+
+
+func _on_area_2d_body_exited(body):
+	if body.has_method("getCanSee"):
+		player_in_area= false
+		player = body
+		player.setCanHide(false)
