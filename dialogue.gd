fa581a52035f598a31c3cfe0ea019c70da98c1f5
iSayli <68780648+iSayli@users.noreply.github.com>
2024-04-22 18:51:08 -0400

    Added option

--- dialogue.gd
+++ dialogue.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # Define an array of texts
-var texts = ["Hey Ania, I have another job for you. ", "My sources have informed me that the Aritish Museum is adding another artifact to its collection in a few days. ", "The artifact in question is Garuda’s feather. ", "Yes, THE Garuda. You’re familiar with the legend, I’m sure. ","Your mission, should you choose to accept, is to infiltrate the high security museum ","repossess the artifact", "replace it with the letter" ,"and return the artifact to the Indian people. ", "May the odds be ever in your favor.."]
+var texts = ["Hey Ania, I have another job for you. ", "I've got new information from our sources" ,"The Aritisg Museum is adding another artifact to its collection in a few days. ", "The artifact in question is Garuda’s feather. ", "Yes, THE Garuda. You’re familiar with the legend, I’m sure. ","Your mission, should you choose to accept, is to infiltrate the high security museum ","repossess the artifact", "replace it with the letter" ,"and return the artifact to the Indian people. ", "May the odds be ever in your favor.."]
 var current_text_index = 0
 
 # Reference to the Label node
@@ -9,7 +9,7 @@ var current_text_index = 0
 @onready var label = $MarginContainer/TextureRect/TextLabel
 @onready var button1 = $TextureButton
 @onready var button2 = $TextureButton2
-
+@onready var button3 = $TextureButton3
 
 func _ready():
 	if text:
@@ -17,6 +17,7 @@ func _ready():
 	if label:
 		label.text = "Operator"
 	button2.hide()
+	button3.hide()
 
 func _on_texture_button_pressed():
 	current_text_index += 1
@@ -24,8 +25,14 @@ func _on_texture_button_pressed():
 		text.text = "Do you accept?"
 		button1.hide()
 		button2.show()
+		button3.show()
 	elif current_text_index < texts.size():
 		text.text = texts[current_text_index]
 
 func _on_texture_button_2_pressed():
 	get_tree().change_scene_to_file("res://main.tscn") # Replace with function body.
+
+
+func _on_texture_button_3_pressed():
+	text.text = "Really? You don’t want to fight systemic colonialism?"
+	button3.hide()
